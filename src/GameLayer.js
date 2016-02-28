1a76ba5fafa53749fb28af8a985bb459e12df6cd
Soraya Saenna <bubblebitoey@gmail.com>
2016-02-28 20:18:13 +0700

    Add Acceleration and Velocity

--- src/GameLayer.js
+++ src/GameLayer.js
@@ -1,14 +1,34 @@
 var GameLayer = cc.LayerColor.extend({
   init: function() {
-    this._super(new cc.Color(39, 48, 72, 255));
+    this._super(new cc.Color(240, 220, 175, 255));
     this.setPosition(new cc.Point(0, 0));
+
     // add player in the child
     this.player = new Player();
     this.player.setPosition(new cc.Point(screenWidth / 2, screenHeight / 2));
     this.addChild(this.player);
+
+    // report position
+    this.positionLabel = cc.LabelTTF.create("", 'Arial', 40);
+    this.positionLabel.setPosition(new cc.Point(screenWidth - 100, screenHeight - 50));
+    this.addChild(this.positionLabel);
+
+    this.scheduleUpdate();
     this.player.scheduleUpdate();
 
     return true;
+  },
+  update: function() {
+    var pos = this.player.getPosition()
+    if (pos.y < 0 || pos.y > screenHeight) {
+      this.player.unscheduleUpdate();
+      this.unscheduleUpdate();
+    }
+    if (pos.x < 0 || pos.x > screenWidth) {
+      this.player.unscheduleUpdate();
+      this.unscheduleUpdate();
+    }
+    console.log(pos);
   }
 });
 
--- src/Player.js
+++ src/Player.js
@@ -2,5 +2,12 @@ var Player = cc.Sprite.extend({
   ctor: function() {
     this._super();
     this.initWithFile("res/Images/dot.png");
-  }
+
+    this.vy = 15;
+  },
+  update: function( dt ) {
+        var pos = this.getPosition();
+        this.setPosition( new cc.Point( pos.x, pos.y + this.vy ) );
+        this.vy += -1;
+    }
 })
