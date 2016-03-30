39a4a57644bf842a67094c4eb1434d0ba189ac59
Soraya Saenna <bubblebitoey@gmail.com>
2016-03-30 21:47:35 +0700

    obstacle can add by press "a" button and show status by press "s".

--- src/GameLayer.js
+++ src/GameLayer.js
@@ -1,5 +1,6 @@
 /**
  * @author ""
+ * @
  * @version 1.5.0
  */
 
@@ -30,6 +31,7 @@ var GameLayer = cc.LayerColor.extend({
 
         this.liveLabel = cc.LabelTTF.create(this.player.live, 'Arial', 40);
         this.liveLabel.setPosition(new cc.Point(screenWidth - 40, screenHeight - 100));
+        this.liveLabel.setColor(cc.color(0, 0, 255));
         this.addChild(this.liveLabel);
 
         this.addKeyboardHandlers();
@@ -60,27 +62,6 @@ var GameLayer = cc.LayerColor.extend({
                 }
             }
 
-            // dead player
-            if (this.state == GameLayer.STATES.DEAD) {
-                // stop player
-                this.player.stop();
-                // stop obstacle
-                for (var i = 0; i < this.somethings.length; i++) {
-                    this.somethings[i].stop();
-                }
-
-                if (confirm("Do you want to play again!?")) {
-                    this.restart();
-                } else {
-                    this.endLabel = cc.LabelTTF.create("GAME IS END", 'Arial', 100);
-                    this.endLabel.setPosition(new cc.Point(screenWidth / 2, screenHeight / 2));
-                    this.endLabel.setColor(cc.color(255, 0, 0));
-                    this.addChild(this.endLabel);
-
-                    this.state = GameLayer.STATES.END;
-                }
-            }
-
             // Play iT!
             if (this.state == GameLayer.STATES.STARTED) {
                 this.player.start();
@@ -92,13 +73,15 @@ var GameLayer = cc.LayerColor.extend({
                 for (var i = 0; i < this.somethings.length; i++) {
                     if (this.player.hit(this.somethings[i])) {
                         // loss live.. another way of if.
-                        this.player.lossLive() ? this.state = GameLayer.STATES.DEAD : this.state = GameLayer.STATES.STARTED;
-                        // obslacle
+                        this.player.lossLive() ? this.endGame() : this.state = GameLayer.STATES.STARTED;
+                        // set live label
+                        this.liveLabel.setString(this.player.live);
+                        // obstacle
                         this.somethings[i].randomPosition();
                     }
                 }
 
-                // update label
+                // update label and color
                 this.liveLabel.setString(this.player.live);
                 if (this.player.live == 2) {
                     this.liveLabel.setColor(cc.color(255, 255, 0));
@@ -116,12 +99,19 @@ var GameLayer = cc.LayerColor.extend({
         cc.eventManager.addListener({
             event: cc.EventListener.KEYBOARD,
             onKeyPressed: function (keyCode, event) {
-                self.onKeyDown(keyCode, event);
+                self.onKeyDown(keyCode);
             }
         }, this);
     },
 
-    onKeyDown: function (keyCode, event) {
+    addObstacle: function () {
+        this.somethings.push(new Something);
+        this.addChild(this.somethings[this.somethings.length - 1]);
+        this.somethings[this.somethings.length - 1].scheduleUpdate();
+        console.info("Add finish, Have: " + this.somethings.length);
+    },
+
+    onKeyDown: function (keyCode) {
         if (keyCode == cc.KEY.up || keyCode == cc.KEY.right || keyCode == cc.KEY.down || keyCode == cc.KEY.left) {
             if (this.state == GameLayer.STATES.PAUSE) {
                 this.state = GameLayer.STATES.STARTED;
@@ -133,7 +123,30 @@ var GameLayer = cc.LayerColor.extend({
         }
         // check by key
         if (keyCode == cc.KEY.s) {
-            console.info("state: " + this.state);
+            // check state
+            if (this.state == GameLayer.STATES.STARTED) {
+                console.info("state: Started");
+            } else if (this.state == GameLayer.STATES.PAUSE) {
+                console.info("state: Pause");
+            } else if (this.state == GameLayer.STATES.DEAD) {
+                console.info("state: Dead");
+            } else if (this.state == GameLayer.STATES.END) {
+                console.info("state: End");
+            }
+            // check player started
+            console.info("Start Player: " + this.player.started);
+            // check obstacle started
+            this.somethings.forEach(function (something) {
+                console.info("Start Obstacle: " + something.started);
+            }, this);
+            // check num obstacle
+            console.info("Number of obstacle: " + this.somethings.length);
+            // check obstacle
+            console.info(this.somethings);
+        }
+
+        if (keyCode == cc.KEY.a) {
+            this.addObstacle();
         }
     },
 
@@ -148,9 +161,16 @@ var GameLayer = cc.LayerColor.extend({
             this.player.score = 0;
             this.player.live = Player.lIVE;
 
+            // remove obstacle
             for (var i = 0; i < this.somethings.length; i++) {
-                // obstacle
-                this.somethings[i].randomPosition();
+                this.removeChild(this.somethings[i], true);
+            }
+            // clear array
+            this.somethings.splice(0, this.somethings.length);
+
+            // add new object
+            for (var i = 0; i < GameLayer.NUMOBJECT; i++) {
+                this.addObstacle();
             }
         } else {
             console.error("You not DEAD!");
@@ -159,6 +179,22 @@ var GameLayer = cc.LayerColor.extend({
 
     endGame: function () {
         this.state = GameLayer.STATES.DEAD;
+        // stop player
+        this.player.stop();
+        // stop obstacle
+        for (var i = 0; i < this.somethings.length; i++) {
+            this.somethings[i].stop();
+        }
+        if (confirm("Do you want to play again!?")) {
+            this.restart();
+        } else {
+            this.endLabel = cc.LabelTTF.create("GAME IS END", 'Arial', 100);
+            this.endLabel.setPosition(new cc.Point(screenWidth / 2, screenHeight / 2));
+            this.endLabel.setColor(cc.color(255, 0, 0));
+            this.addChild(this.endLabel);
+
+            this.state = GameLayer.STATES.END;
+        }
     }
 });
 
