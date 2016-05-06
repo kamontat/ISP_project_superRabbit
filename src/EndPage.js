faf00c78298a50ba29ea94a0d72d153b2b2844b6
Soraya Saenna <bubblebitoey@gmail.com>
2016-05-07 02:42:17 +0700

    add new file

---
+++ src/EndPage.js
@@ -0,0 +1,10 @@
+/**
+ * Created by bubblebitoey on 7/5/59.
+ */
+
+var EndPage = cc.Sprite.extend({
+    ctor: function() {
+        this._super();
+        this.initWithFile("res/Images/endPage.jpg");
+    }
+})
